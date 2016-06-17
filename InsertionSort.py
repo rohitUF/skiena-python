415ceeb62012eb2b9cb64488f2b060c55396bc0c
rohituf <rohitchauhan.mail@gmail.com>
2016-06-17 08:26:14 +0000

    Added Insertion Sort

---
+++ InsertionSort.py
@@ -0,0 +1,20 @@
+import unittest
+
+def sort(arr):
+    for idx, val in enumerate(arr):
+        pos = idx
+        while pos > 0 and arr[pos-1] > val:
+            arr[pos] = arr[pos-1]
+            pos = pos -1
+        arr[pos] = val
+            
+class TestSorting(unittest.TestCase):
+        
+    def testSorting(self):
+        arr = [4, 3, 5]
+        sort(arr)
+        self.assertEqual(arr, [3, 4, 5])
+        
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSorting)
+    unittest.TextTestRunner(verbosity=2, buffer=False).run(suite)
